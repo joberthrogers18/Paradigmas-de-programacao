7a4537ed91ae0a0627dd09667029b6a4aae8d74a
Joberth Rogers <joberth.rogers18@gmail.com>
2019-04-06 13:23:22 -0400

    Get all the names but, the link for statisc give 403 and cant connect

--- web_scrapping/mathematiciens.py
+++ web_scrapping/mathematiciens.py
@@ -8,9 +8,9 @@ def simple_get(url):
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
                 return resp.content
-                
             else:
                 return None
+                
     except RequestException as e:
         log_error('Error during request to {0} : {1}'.format(url, str(e)))
     
@@ -37,26 +37,50 @@ def get_name():
 
     raise Exception('Error retriving contents at {}'.format(url))
 
+def get_hit_on_name(name):
+
+    url_root = 'https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/'+name
+    response = simple_get(url_root.format(name))
+    
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+
+        hit_link = [a for a in html.select('a')
+                      if a['href'].find('lastest-60') > -1]
+        
+        if len(hit_link) > 0:
+            link_text = hit_link[0].text.replace(',', '')
+            try:
+                return int(link_text)
+            except:
+                log_error("couldn't parse {} as an `int`".format(link_text))
+
+    log_error('No pageviews found for {}'.format(name))
+    return None
 '''
-### testing the beautifulsoup
+if __name__ == '__main__':
+    print('Getting the list of names')
+    names = get_name()
+    print('... done')
 
-raw_html = open('./contrived.html').read()
-html = BeautifulSoup(raw_html, 'html.parser') # make the parse to html
+    results = []
 
-print(type(html))
+    print('Getting stats for each name...')
 
-for p in html.select('p'):
-    if p['id'] == 'walrus':
-        print(p)
+    for name in names:
+        try:
+            hits = get_hit_on_name(name)
+            if hits is None:
+                hits = -1
+            results.append((hits, name))
+        except: 
+            results.append((-1, name))
 
-print('\n\n\n The mathematiciens\n\n')
-
-raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
-html = BeautifulSoup(raw_html, 'html.parser')
-
-for i, li in enumerate(html.select('li')):
-    print(i, li.text)  
+    print('Done...')
+    print(results)
 
 '''
 
-print(get_name())
+resp = get('https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/Mathematical%20optimization/2019-03-15/2019-04-06')
+print(resp.content)
+# raw_html = BeautifulSoup(resp, 'html.parser')
